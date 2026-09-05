5729b75025d96120f9f2a59a1de1181a47f339ee
Numan <33553832+Numi2@users.noreply.github.com>
2026-09-05 03:22:34 +0200

    build(md): bundle minimization Metal module

--- Package.swift
+++ Package.swift
@@ -28,6 +28,7 @@ let package = Package(
                 .copy("Resources/NumiVivoMDNeighborGrid.metal"),
                 .copy("Resources/NumiVivoMDPME.metal"),
                 .copy("Resources/NumiVivoMDPMECorrections.metal"),
+                .copy("Resources/NumiVivoMDMinimization.metal"),
                 .copy("Resources/NumiVivoExactSSAKernels.metal"),
                 .copy("Resources/NumiVivoMigrationKernels.metal"),
                 .copy("Resources/NumiVivoPartitionKernels.metal"),
