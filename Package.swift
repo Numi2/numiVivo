1f2b5b1ec4b69fe4f995408f6e06b84a9d38c02d
Numan <33553832+Numi2@users.noreply.github.com>
2026-09-05 03:10:26 +0200

    build(md): bundle spatial neighbor-grid Metal module

--- Package.swift
+++ Package.swift
@@ -25,6 +25,7 @@ let package = Package(
                 .copy("Resources/NumiVivoPhysiologyKernels.metal"),
                 .copy("Resources/NumiVivoMDKernels.metal"),
                 .copy("Resources/NumiVivoMDVirtualSites.metal"),
+                .copy("Resources/NumiVivoMDNeighborGrid.metal"),
                 .copy("Resources/NumiVivoExactSSAKernels.metal"),
                 .copy("Resources/NumiVivoMigrationKernels.metal"),
                 .copy("Resources/NumiVivoPartitionKernels.metal"),
